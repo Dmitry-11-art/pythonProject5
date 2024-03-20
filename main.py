41e026cc7a6449e17ab58d31ffc638449b126e47
Acer <seleznev18012003s@gmail.com>
2024-03-20 16:21:29 +0500

    commit2

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def fib(n):
    if n <= 2:
        return 1
    return fib(n-1)+fib(n-2)
 print(fib(10))
+
+
+print("Hello Github!!!!!!!!!!!!!!!1")
+
+def func1(x, y):
+    print(x)
+    print(y)
+    return x**2 + y**2
+
+a = 4
+b = 5
+print(func1(a, b))
